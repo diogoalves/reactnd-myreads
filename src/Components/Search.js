dccf2f825fb3f2ebc388d2df2eaa11fd5624d420
Diogo Alves <diogo.alves@gmail.com>
2017-11-12 19:23:01 -0200

    feat: Add search using BooksAPI

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import escapeRegExp from 'escape-string-regexp';
+// import sortBy from 'sort-by';
+import Book from './Book';
+import * as BooksAPI from '../utils/BooksAPI';
 
+// todo add debounce to search
 class Search extends Component {
 
+  state = {
+    books: [],
+    query: ''
+  }
+
+  handleChange = (event) => {
+    const query = event.target.value.trim();
+
+    this.setState({
+      query
+    })
+
+    BooksAPI.search(query).then((books) => {
+      if(Array.isArray(books)) {
+        this.setState({ books });
+      } else {
+        this.setState({ books: [] });
+      }
+    })
+  }
+
   render() {
+    const { books, query } = this.state;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          {/* <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a> */}
           <Link to='/' className='close-search'>Close</Link>
           <div className="search-books-input-wrapper">
             {/*
@@ -18,12 +45,16 @@ class Search extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text" value={query} onChange={this.handleChange} placeholder="Search by title or author"/>
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books.map( (book) => (
+              <Book key={book.id} data={book} />
+            ))}
+          </ol>
         </div>
       </div>
     );
